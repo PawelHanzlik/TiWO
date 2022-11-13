836e2010a360fc06ec5c7f7b1e1411d704510447
PawelHanzlik <hanzlik_pawel@o2.pl>
2022-11-13 13:57:29 +0100

    Initial commit

---
+++ src/main/java/ph/agh/tiwo/TiwoApplication.java
@@ -0,0 +1,13 @@
+package ph.agh.tiwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ph/agh/tiwo/TiwoApplicationTests.java
@@ -0,0 +1,13 @@
+package ph.agh.tiwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
